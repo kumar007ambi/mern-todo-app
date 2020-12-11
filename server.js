7f37e4f827b1533b56dbd8534278184faed7565b
Ambika <nirajkewat011@gmail.com>
2020-12-11 16:38:56 +0530

    make it better

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const host = '0.0.0.0';
-const port = (process.env.port || '0.0.0.0');
+
+const port = process.env.PORT || 5000;
 
 
-app.listen(port, host, () => {
+app.listen(port, () => {
     console.log('Server is started on Port' + port)
 })
